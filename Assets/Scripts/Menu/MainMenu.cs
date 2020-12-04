c10c80d280f52cca1c54c55d80afde97eb80485e
Big_Earl <477117@chrome.escg.ac.uk>
2020-12-04 16:07:38 +0000

    New death screen and menu code test

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene("Museum");
+
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Start");
+
+    }
+
+    public void Quit()
+    {
+
+        Application.Quit();
+
+        Debug.Log("Has quitted");
+        
+    }
+
+
+
+
+
+}
